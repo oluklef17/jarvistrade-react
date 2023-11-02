0132c18514aad16659682e45015b177af8e5e7a3
Oluyemi Sodipo <89250614+oluklef17@users.noreply.github.com>
2023-11-02 10:02:48 +0100

    Multiple updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTelegram,
-  faTwitter,
-  faWhatsapp,
-  faYoutube,
-} from "@fortawesome/free-brands-svg-icons";
+import { FaTelegram, FaWhatsapp, FaTwitter, FaYoutube } from "react-icons/fa";
 import "../styles/style.css";
 
 function Contact() {
@@ -17,20 +11,20 @@ function Contact() {
         Reach us on any of our social media platforms
       </h1>
       <a href="https://t.me/oluklef17" id="telegram" target="_blank">
-        <FontAwesomeIcon icon={faTelegram} className="sm-icon" />
+        <FaTelegram className="sm-icon" />
       </a>
       <a href="https://wa.me/+2348072708424" id="whatsapp" target="_blank">
-        <FontAwesomeIcon icon={faWhatsapp} className="sm-icon" />
+        <FaWhatsapp className="sm-icon" />
       </a>
       <a href="https://twitter.com/jarvistrade_ai" id="twitter" target="_blank">
-        <FontAwesomeIcon icon={faTwitter} className="sm-icon" />
+        <FaTwitter className="sm-icon" />
       </a>
       <a
         href="https://www.youtube.com/@jarvistrade6558"
         id="youtube"
         target="_blank"
       >
-        <FontAwesomeIcon icon={faYoutube} className="sm-icon" />
+        <FaYoutube className="sm-icon" />
       </a>
     </>
   );
